29f6cf2c338b7800c0f7e70ed8320fe1117a58fd
sonyxme <bogdanova_02@mail.ru>
2023-03-25 17:01:52 +0300

    edit functions

--- deleteChats.py
+++ deleteChats.py
@@ -5,7 +5,34 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 api_id = int(config['TELEGRAM']['api_id'])
 api_hash = config['TELEGRAM']['api_hash']
-client = TelegramClient('account', api_id, api_hash)
+client = TelegramClient('telethon', api_id, api_hash)
+
+list_to_del = []
+
+
+# async def delete_chats():
+#     c = 0
+#     for el in list_to_del:
+#         await client.delete_dialog(el)
+#         c += 1
+#     print(f'{c} chats have been deleted')
+
+
+async def dry_run():
+    print(f'The list of IDs for deleting chats: {list_to_del}')
+    action = input('Would you like to delete these chats? (yes/no): ')
+    while action not in ('yes', 'no'):
+        print('Invalid response, enter again')
+        action = input('Would you like to delete these chats? (yes/no): ')
+    if action == 'yes':
+        c = 0
+        for el in list_to_del:
+            await client.delete_dialog(el)
+            c += 1
+        print(f'{c} chats have been deleted')
+        # await delete_chats()
+    else:
+        exit()
 
 
 async def main():
@@ -13,13 +40,15 @@ async def main():
     async for dialog in client.iter_dialogs():
         try:
             if dialog.entity.deleted:
-                print(f'The ID of the deleted account: {dialog.entity.id}')
+                list_to_del.append(dialog.entity.id)
                 k = False
-                await client.delete_dialog(dialog)
+                # await client.delete_dialog(dialog)
         except Exception:
             continue
     if k:
         print('Deleted accounts were not found.')
+    else:
+        await dry_run()
 
 
 if __name__ == '__main__':
