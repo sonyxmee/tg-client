a943960c119c1befda0d02f853269f935ca1c347
sonyxme <bogdanova_02@mail.ru>
2023-04-11 15:26:33 +0300

    edit files

--- folderNotify.py
+++ folderNotify.py
@@ -7,23 +7,29 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 api_id = int(config['TELEGRAM']['api_id'])
 api_hash = config['TELEGRAM']['api_hash']
-client = TelegramClient('account', api_id, api_hash)
+client = TelegramClient('account_telethon', api_id, api_hash)
 
 
 async def main(folder_title):
     peers = []
+    flag = False
     folders = await client(functions.messages.GetDialogFiltersRequest())
 
     for folder in folders[1:]:
         if folder.title == folder_title:
+            flag = True
             try:
                 # добавляю все каналы и чаты папки в список
                 peers.extend(folder.pinned_peers)
                 peers.extend(folder.include_peers)
-                peers.extend(folder.exclude_peers)
                 break
             except Exception:
                 continue
+
+    if not flag:
+        print("Folder is not found.")
+        return
+
     for peer in peers:
         await client(functions.account.UpdateNotifySettingsRequest(
             peer=peer,
